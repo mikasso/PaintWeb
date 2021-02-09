5c2ee3789d55aa00cae8dab9391ab98eb952de52
mikasso <mikokaj.szaroleta@gmail.com>
2021-02-09 13:38:58 +0100

    dividing javascript file

---
+++ Paintweb/wwwroot/js/CanvasManager.js
@@ -0,0 +1,68 @@
+﻿function Point(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+function Line(p1 = null, p2 = null, color = "green") {
+    this.p1 = p1;
+    this.p2 = p2;
+    this.color = color;
+}
+
+function CanvasManager(canvas) {
+    this.canvas = canvas;
+    this.HTMLelement = canvas[0];
+    this.ctx = this.HTMLelement.getContext("2d");
+    this.paint = false;
+    this.line = new Line(0, 0);
+    this.hub = {};
+
+    this.drawLine = function (line) {
+        this.ctx.strokeStyle = line.color;
+        this.ctx.lineJoin = "round";
+        this.ctx.lineWidth = 2;
+        this.ctx.beginPath();
+        this.ctx.moveTo(line.p1.x, line.p1.y);
+        this.ctx.lineTo(line.p2.x, line.p2.y);
+        this.ctx.closePath();
+        this.ctx.stroke();
+    };
+
+    this.getOffset = function () {
+        const rect = this.HTMLelement.getBoundingClientRect();
+        return {
+            left: rect.left + window.scrollX,
+            top: rect.top + window.scrollY
+        };
+    };
+
+    this.getPointFromEvent = function (event) {
+        const offset = this.getOffset();
+        return new Point(
+            event.pageX - offset.left,
+            event.pageY - offset.top);
+    };
+
+    this.canvas.on('mousedown', () => {
+        console.log(event);
+        this.line.p1 = this.getPointFromEvent(event);
+        this.paint = true;
+    });
+
+    this.canvas.on('mouseup', () => {
+        this.paint = false
+        console.log("Mouse up");
+    });
+
+    this.canvas.on('mousemove', (event) => {
+        if (this.paint == false)
+            return;
+        this.line.p2 = this.getPointFromEvent(event);
+        if (this.hub != null)
+            if (this.hub.isConnected == true) {
+                this.drawLine(this.line);
+                this.hub.sendLine(this.line);
+            }
+        this.line.p1 = this.line.p2;
+    });
+}
---
+++ Paintweb/wwwroot/js/Hub.js
@@ -0,0 +1,66 @@
+﻿function Hub(canvasManager) {
+    this.user = "user_1";
+    this.group = "g1";
+    this.connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+    this.canvasManager = canvasManager;
+    this.isConnected = false;
+
+    this.connection.on("ReceiveLine", (user, message) => {
+        this.canvasManager.drawLine(JSON.parse(message));
+    });
+
+    this.connection.on("ClearCanvas", (user, message) => {
+        console.log("Clear");
+        clearCanvas();
+    });
+
+    this.connection.on("ReceiveText", (user, message) => {
+        console.log(message);
+    });
+
+}
+
+Hub.prototype.sendLine = function (line) {
+    if (this.isConnected == true) {
+        this.connection.invoke("SendLineToGroup", this.user, JSON.stringify(line)).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
+};
+
+Hub.prototype.disconnect = function () {
+    if (this.group != "")
+        this.connection.invoke("RemoveFromGroup", this.user, this.group).catch(function (err) {
+            alert(err.toString());
+        });
+}
+
+
+Hub.prototype.JoinGroup = function (groupName) {
+    this.group = groupName;
+    this.connection.invoke("AddToGroup", this.user, groupName).catch(function (err) {
+        return console.error(err.toString());
+    });
+}
+
+Hub.prototype.SynchronizeCanvas = function () {
+    this.connection.invoke("SynchronizeCanvas", this.user, "synchronizecanvas").catch(function (err) {
+        return console.error(err.toString());
+    });
+}
+
+Hub.prototype.RequestClearCanvas = function () {
+    var message = "ClearCanvas";
+    this.connection.invoke("SendClear", this.user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+}
+
+Hub.prototype.init = function () {
+    this.connection.start().then( () => {
+        this.isConnected = true;
+        groupName = document.getElementById("GroupTextInput").value;
+        this.JoinGroup(groupName);
+        this.SynchronizeCanvas();
+    });
+}
